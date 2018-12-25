da1f342837c567ee49323b65cb8efa481a9d4d6a
Mickle Isaev <mrraptor26@gmail.com>
2018-12-25 14:43:31 +0300

    add new funcitons

--- Lib_A_PEC_position_error_compensation.c
+++ Lib_A_PEC_position_error_compensation.c
@@ -1,14 +1,14 @@
-/** 
+/**
  * @file   	%<%NAME%>%.%<%EXTENSION%>%
  * @author 	Mickle Isaev
- * @version	
+ * @version
  * @date 	%<%DATE%>%, %<%TIME%>%
  * @brief
  */
 
 
 /*#### |Begin| --> Секция - "Include" ########################################*/
-#include "PEC_position_error_compensation.h"
+#include "Lib_A_PEC_position_error_compensation.h"
 /*#### |End  | <-- Секция - "Include" ########################################*/
 
 
@@ -21,14 +21,125 @@
 
 
 /*#### |Begin| --> Секция - "Прототипы локальных функций" ####################*/
+static void
+PEC_NED2ECEF(
+	pec_all_data_s *p_s,
+	__PEC_FPT__ vectInNED_a[],
+	__PEC_FPT__ VectInECEF_a[],
+	__PEC_FPT__ lat,
+	__PEC_FPT__ lon);
 /*#### |End  | <-- Секция - "Прототипы локальных функций" ####################*/
 
 
 /*#### |Begin| --> Секция - "Описание глобальных функций" ####################*/
+void
+PEC_Init(
+	pec_all_data_s *p_s,
+	pec_all_data_init_s *pInit_s,
+	__PEC_FPT__ integratePeriodInSec,
+	__PEC_FPT__ lat,
+	__PEC_FPT__ lon)
+{
+	ninteg_trapz_InitStruct_s 	  init_s;
+	NINTEG_Trapz_StructInit		(&init_s);
+
+	init_s.accumulate_flag = NINTEG_DISABLE;
+	init_s.integratePeriod = (__NUNTEG_FPT__) integratePeriodInSec;
+
+	size_t i = 0;
+	for (i = 0; i <= PEC_ECEF_AXIS_NUMB; i++)
+	{
+		/* Инициализация структуры для интегрирования ускорения */
+		NINTEG_Trapz_Init(
+			&p_s->velosityInNED_s.acc2VelIntegrate_s_a[i],
+			&init_s);
+
+		/* Инициализация структуры для интегрирования скорости */
+		NINTEG_Trapz_Init(
+			&p_s->positionInECEF_s.vel2PosIntegrate_s_a[i],
+			&init_s);
+	}
+
+	p_s->lat = lat;
+	p_s->lon = lon;
+}
+
+void
+PEC_GetVelosityEstimate(
+	pec_all_data_s *p_s,
+	__PEC_FPT__ *pAccelerationInNED)
+{
+	/* Интегрирование ускорений по трем осям для получения оценки скорости */
+	size_t i = 0;
+	for (i = 0; i <= PEC_ECEF_AXIS_NUMB; i++)
+	{
+		p_s->velosityInNED_s.vel_a[i] +=
+			NINTEG_Trapz(
+				&p_s->velosityInNED_s.acc2VelIntegrate_s_a[i],
+				(__NUNTEG_FPT__) * pAccelerationInNED++);
+	}
+}
+
+void
+PEC_Get_PositionEstimate(
+	pec_all_data_s *p_s)
+{
+	/* Выполнение проекции вектора скорости из NED в ECEF */
+	__PEC_FPT__ velosityInECEF_a[PEC_ECEF_AXIS_NUMB];
+	PEC_NED2ECEF(
+		p_s,
+		p_s->velosityInNED_s.vel_a,
+		velosityInECEF_a,
+		p_s->lat,
+		p_s->lon);
+
+	/* Выполнение интегрирование вектора скорости в ECEF системе координат */
+	size_t i = 0;
+	for (i = 0; i <= PEC_ECEF_AXIS_NUMB; i++)
+	{
+		p_s->positionInECEF_s.pos_a[i] +=
+			NINTEG_Trapz(
+				&p_s->positionInECEF_s.vel2PosIntegrate_s_a[i],
+				(__NUNTEG_FPT__) velosityInECEF_a[i]);
+	}
+}
+
+void
+PEC_CorrectVelosity(
+	pec_all_data_s *p_s,
+	__PEC_FPT__ velosityMeasurements_a[])
+{
+	
+}
 /*#### |End  | <-- Секция - "Описание глобальных функций" ####################*/
 
 
 /*#### |Begin| --> Секция - "Описание локальных функций" #####################*/
+void
+PEC_NED2ECEF(
+	pec_all_data_s *p_s,
+	__PEC_FPT__ vectInNED_a[],
+	__PEC_FPT__ VectInECEF_a[],
+	__PEC_FPT__ lat,
+	__PEC_FPT__ lon)
+{
+	__PEC_FPT__ temp =
+		p_s->trigFnc_s.pFncCos(lat) * (-vectInNED_a[PEC_NED_Z])
+		- (p_s->trigFnc_s.pFncSin(lat) * vectInNED_a[PEC_NED_X]);
+
+	VectInECEF_a[PEC_ECEF_X] =
+		(p_s->trigFnc_s.pFncCos(lon) * temp )
+		- (p_s->trigFnc_s.pFncSin(lon) * vectInNED_a[PEC_NED_Y]);
+
+	VectInECEF_a[PEC_ECEF_Y] =
+		(p_s->trigFnc_s.pFncSin(lon) * temp )
+		- (p_s->trigFnc_s.pFncCos(lon) * vectInNED_a[PEC_NED_Y]);
+
+	VectInECEF_a[PEC_ECEF_Z] =
+		p_s->trigFnc_s.pFncSin(lat) * (-vectInNED_a[PEC_NED_Z])
+		+  p_s->trigFnc_s.pFncCos(lat) * vectInNED_a[PEC_NED_X];
+
+}
 /*#### |End  | <-- Секция - "Описание локальных функций" #####################*/
 
 
--- Lib_A_PEC_position_error_compensation.h
+++ Lib_A_PEC_position_error_compensation.h
@@ -7,27 +7,96 @@
  */
 
 
-#ifndef PEC_POSITION_ERROR_COMPENSATION_H_
-#define PEC_POSITION_ERROR_COMPENSATION_H_
+#ifndef LIB_A_PEC_POSITION_ERROR_COMPENSATION_H_
+#define LIB_A_PEC_POSITION_ERROR_COMPENSATION_H_
 
 
 /*#### |Begin| --> Секция - "Include" ########################################*/
 /*==== |Begin| --> Секция - "C libraries" ====================================*/
+#include <stdint.h>
+#include <stdio.h>
+#include <math.h>
 /*==== |End  | <-- Секция - "C libraries" ====================================*/
 
 /*==== |Begin| --> Секция - "MK peripheral libraries" ========================*/
 /*==== |End  | <-- Секция - "MK peripheral libraries" ========================*/
 
 /*==== |Begin| --> Секция - "Extern libraries" ===============================*/
+#include "Lib_A_NINTEG_numerical_integration/Lib_A_NINTEG_numerical_integration.h"
+#include "Lib_A_FILT_filters.c/Lib_A_FILT_filters.h"
 /*==== |End  | <-- Секция - "Extern libraries" ===============================*/
 /*#### |End  | <-- Секция - "Include" ########################################*/
 
 
 /*#### |Begin| --> Секция - "Определение констант" ###########################*/
+#if !defined (__PEC_FPT__)
+#error 'Please, set PEC_FPT float or double'
+#endif
 /*#### |End  | <-- Секция - "Определение констант" ###########################*/
 
 
 /*#### |Begin| --> Секция - "Определение типов" ##############################*/
+typedef enum
+{
+	PEC_ECEF_X = 0,
+	PEC_ECEF_Y,
+	PEC_ECEF_Z,
+
+	PEC_ECEF_AXIS_NUMB,
+} pec_ecef_coordinate_system_e;
+
+typedef enum
+{
+	PEC_NED_X = 0,
+	PEC_NED_Y,
+	PEC_NED_Z,
+
+	PEC_NED_AXIS_NUMB,
+} pec_ned_coordinate_system_e;
+
+typedef struct
+{
+	__PEC_FPT__ (*pFncSin)(
+			__PEC_FPT__ val);
+
+	__PEC_FPT__ (*pFncCos)(
+			__PEC_FPT__ val);
+
+} pec_trigonometric_fnc_pointers_s;
+
+typedef struct
+{
+	ninteg_trapz_s 	acc2VelIntegrate_s_a[PEC_NED_AXIS_NUMB];
+
+	FILT_comp_filt_s compFilt_s_a[PEC_NED_AXIS_NUMB];
+
+	/* Оценка скорости в NED системе координат */
+	__PEC_FPT__  	vel_a[3];
+} pec_velosity_ned_s;
+
+typedef struct
+{
+	ninteg_trapz_s 	vel2PosIntegrate_s_a[PEC_ECEF_AXIS_NUMB];
+	FILT_comp_filt_s compFilt_s_a[PEC_ECEF_AXIS_NUMB];
+
+	/* Оценка местоположения в ECEF системе координат */
+	__PEC_FPT__  	pos_a[3];
+} pec_position_ecef_s;
+
+typedef struct
+{
+	pec_trigonometric_fnc_pointers_s trigFnc_s;
+	pec_velosity_ned_s 		velosityInNED_s;
+	pec_position_ecef_s 	positionInECEF_s;
+	__PEC_FPT__ lat;
+	__PEC_FPT__ lon;
+} pec_all_data_s;
+
+typedef struct
+{
+	__PEC_FPT__ velosityCorrectCoeff;
+	__PEC_FPT__ positionCorrectCoeff;
+} pec_all_data_init_s;
 /*#### |End  | <-- Секция - "Определение типов" ##############################*/
 
 
@@ -42,7 +111,7 @@
 /*#### |Begin| --> Секция - "Определение макросов" ###########################*/
 /*#### |End  | <-- Секция - "Определение макросов" ###########################*/
 
-#endif	/* PEC_POSITION_ERROR_COMPENSATION_H_ */
+#endif	/* LIB_A_PEC_POSITION_ERROR_COMPENSATION_H_ */
 
 /*############################################################################*/
 /*################################ END OF FILE ###############################*/
